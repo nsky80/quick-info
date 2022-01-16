30f24415d4be2db783495bb2b1b8ff5bd585337a
Satish NSKY80 <satishkumary80@gmail.com>
2022-01-16 19:15:23 +0530

    about and interests added

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,10 @@
-export default function About(){
+export default function About() {
     return (
-        <div className="info">
+        <div className="about-interests">
             <h3>About</h3>
-            <p>Lorum Ipsum dorom sit..</p>
+            <p>Chania is the capital of the Chania region on the island of Crete. The city can be divided in two parts, the old town and the modern city.
+
+            </p>
         </div>
     )
 }
--- src/components/Interests.js
+++ src/components/Interests.js
@@ -1,8 +1,8 @@
-export default function Interests(){
+export default function Interests() {
     return (
-        <div className="info">
+        <div className="about-interests">
             <h3>Interests</h3>
-            <p>Lorum Ipsum dorom sit..</p>
+            <p>Spring is a lightweight framework. It can be thought of as a framework of frameworks because it provides support to various frameworks such as Struts</p>
         </div>
     )
 }
